f077a55c787adc6f9fbe7ce3fc3de6a57987d53e
Park Darae <ret7258@naver.com>
2026-05-04 19:32:37 +0900

    fix: 이미지 오류 수정

--- src/NotFound.jsx
+++ src/NotFound.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import image from "/src/images/YU_LikeLion.png";
+import image from "/src/images/404.jpeg";
 
 const NotFound = () => {
     const navigate = useNavigate();
